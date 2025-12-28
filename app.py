fbd0c6b05cf2376ba94a0d1e837a4ebba0f56b6f
anshm1425 <anshm1425@gmail.com>
2025-12-28 16:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
 
 model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+
+st.cache(model.fit(X,y))
 house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House price'):
@@ -39,6 +40,7 @@ msg = f'''House Price is: ${round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design & Developed by: ANSH MISHRA**''')
+
 
 
 
